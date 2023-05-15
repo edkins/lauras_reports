c0faf4a4da73ba003300c057e088680f4fa79904
Giles Edkins <edkins@gmail.com>
2023-05-15 19:55:11 -0400

    Add camera window

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
+import { Camera, CameraType } from 'expo-camera';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [hasPermission, setHasPermission] = Camera.useCameraPermissions();
   return (
     <View style={styles.container}>
+      {
+        hasPermission ?
+          <Camera style={styles.camera} type={type}>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Flip Camera"
+                onPress={() => {
+                  setType(
+                    type === CameraType.back
+                      ? CameraType.front
+                      : CameraType.back
+                  );
+                }}
+              />
+            </View>
+          </Camera>
+        :
+          <Text>No access to camera</Text>
+      }
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -16,5 +39,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    width: 300,
+    height: 300,
   },
 });
